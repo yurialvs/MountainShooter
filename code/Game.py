2f4dc8f74333c08885cc8e95fd6a1aeb4bc130cb
Yuri <lops.yuri@gmail.com>
2024-11-07 13:35:34 -0300

    update

--- code/Game.py
+++ code/Game.py
@@ -2,21 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import pygame
+
+from code.Const import WIN_HEIGHT
+from code.Const import WIN_WIDTH
 from code.Menu import Menu
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            # Check for all events
-            # for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:
-            #       pygame.quit() # Close Window
-            #       quit() # End pygame
